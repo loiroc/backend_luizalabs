2c5bb27dcee3b1db1d725a731471be18975ab4b2
Lucas Coimbra <loiroc@hotmail.com>
2021-06-20 19:03:48 -0300

    add new route which get costumer by id

--- src/controllers/CostumersController.js
+++ src/controllers/CostumersController.js
@@ -9,6 +9,18 @@ class CostumerController {
       return res.status(400);
     }
   }
+  async getById(req, res) {
+    try {
+      const id = req.params.id;
+      const sql = await database.query(
+        `SELECT * FROM costumers WHERE id = ${id}`
+      );
+      if (sql[0].length > 0) return res.send(sql[0]);
+      return res.status(404).send("Costumers does not exist!");
+    } catch (err) {
+      return res.status(400);
+    }
+  }
 }
 
 module.exports = new CostumerController();
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = require('express').Router();
 const CostumerController = require("./controllers/CostumersController")
 
 routes.get('/costumers/', CostumerController.get)
+routes.get('/costumers/:id', CostumerController.getById);
 
 module.exports = routes;
